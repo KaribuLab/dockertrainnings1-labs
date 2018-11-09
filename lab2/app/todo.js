72a313784a25fde89289815b589abe368991a3ed
Patricio Ascencio <patricio.ascencio@karibu.cl>
2018-11-09 17:21:48 -0300

    [REL]: Integración con mongo db y auto restart con pm2

--- lab2/app/todo.js
+++ lab2/app/todo.js
@@ -2,9 +2,25 @@ const mongoose = require('mongoose');
 
 module.exports = () => {
 
-    const dbHost = process.env.DBHOST | 'localhost';
+    const dbHost = process.env.DBHOST || 'localhost';
+    const options = {
+        socketTimeoutMS: 0,
+        keepAlive: true,
+        reconnectTries: 30,
+    };
 
-    mongoose.connect(`mongodb://${dbHost}/todo`);
+    const uri = `mongodb://${dbHost}:27017/todo`;
+
+    console.log('URI: ', uri);
+
+    mongoose.connect(uri, options).then(
+        () => {
+            console.log('Conexion exitosa')
+        },
+        err => {
+            console.log('Error al conectar a base de datos: ', err)
+        }
+    );
 
     return mongoose.model('Todo', {
         name: String,
